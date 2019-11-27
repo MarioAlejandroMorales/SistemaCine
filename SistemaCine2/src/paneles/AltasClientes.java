6e0c7d88037ee566c223baf6095c2e9aea8d67bf
Jaramillo-Karen <57342215+Jaramillo-Karen@users.noreply.github.com>
2019-11-26 21:17:01 -0600

    Add files via upload

---
+++ SistemaCine2/src/paneles/AltasClientes.java
@@ -0,0 +1,150 @@
+package paneles;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import clases.conexionBD;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Random;
+import java.awt.event.ActionEvent;
+import javax.swing.JRadioButton;
+
+@SuppressWarnings("serial")
+public class AltasClientes extends JFrame {
+	
+	private JPanel contentPane;
+	private JTextField textFieldNombre;
+	private conexionBD conexionBD = new conexionBD();
+	private JTextField textFieldApellido;
+	private JLabel lblTelefono;
+	private JTextField textFieldTelefono;
+	private JLabel lblCorreo;
+	private JTextField textFieldCorreo;
+	private JLabel label;
+	private JLabel label_1;
+	private JButton btnAtras;
+	
+	/**
+	 * Create the frame.
+	 */
+	public Altas() {
+		setTitle("Systemovey (Altas Clientes)");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0,0,599,650);
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		label = new JLabel("ALTAS DE CLIENTES");
+		label.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		label.setBounds(167, 13, 246, 36);
+		contentPane.add(label);
+		
+		label_1 = new JLabel("Datos personales");
+		label_1.setBounds(12, 93, 120, 16);
+		contentPane.add(label_1);
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setBounds(12, 122, 120, 16);
+		contentPane.add(lblNombre);
+		
+		textFieldNombre = new JTextField();
+		textFieldNombre.setBounds(147, 122, 116, 22);
+		contentPane.add(textFieldNombre);
+		textFieldNombre.setColumns(10);
+		
+		JLabel lblApelido = new JLabel("Apellido:");
+		lblApelido.setBounds(275, 125, 120, 16);
+		contentPane.add(lblApelido);
+		
+		textFieldApellido = new JTextField();
+		textFieldApellido.setColumns(10);
+		textFieldApellido.setBounds(404, 122, 116, 22);
+		contentPane.add(textFieldApellido);
+		
+		lblTelefono = new JLabel("Telefono:");
+		lblTelefono.setBounds(12, 160, 120, 16);
+		contentPane.add(lblTelefono);
+		
+		textFieldTelefono = new JTextField();
+		textFieldTelefono.setColumns(10);
+		textFieldTelefono.setBounds(147, 157, 116, 22);
+		contentPane.add(textFieldTelefono);
+		
+		lblCorreo = new JLabel("Correo:");
+		lblCorreo.setBounds(272, 160, 120, 16);
+		contentPane.add(lblCorreo);
+		
+		textFieldCorreo = new JTextField();
+		textFieldCorreo.setColumns(10);
+		textFieldCorreo.setBounds(404, 157, 116, 22);
+		contentPane.add(textFieldCorreo);
+		
+		JLabel lblMembresia = new JLabel("Membresia:");
+		lblMembresia.setBounds(275, 227, 120, 16);
+		contentPane.add(lblMembresia);
+		
+		JRadioButton rdbtnF = new JRadioButton("F");
+		rdbtnF.setBounds(404, 220, 45, 25);
+		contentPane.add(rdbtnF);
+		
+		JRadioButton rdbtnM = new JRadioButton("SF");
+		rdbtnM.setBounds(466, 220, 45, 25);
+		contentPane.add(rdbtnM);
+		
+		
+		JButton btnDarDeAlta = new JButton("Dar de alta");
+		btnDarDeAlta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String nombre=textFieldNombre.getText();
+				String apellido=textFieldApellido.getText();
+				String telefono=textFieldTelefono.getText();
+				String correo=textFieldCorreo.getText();
+				String membresia="";
+				if(rdbtnF.isSelected()) {
+					membresia="F";
+				}else{
+					if(rdbtnM.isSelected()) {
+						membresia="SF";
+					}
+				}
+				Random r = new Random();
+				int id_r = r.nextInt(10000);
+				String id=String.valueOf(id_r);
+				
+				try {
+					conexionBD.insertEmpleado(nombre, apellido, telefono, correo, membresia, id);
+					JOptionPane.showInternalMessageDialog(contentPane, "El ID es: "+id);
+					dispose();
+				} catch (SQLException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		btnDarDeAlta.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnDarDeAlta.setBounds(227, 541, 126, 25);
+		contentPane.add(btnDarDeAlta);
+		
+		btnAtras = new JButton("Atras");
+		btnAtras.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		});
+		btnAtras.setBounds(423, 542, 97, 25);
+		contentPane.add(btnAtras);
+		
+	}
+}
---
+++ SistemaCine2/src/paneles/ModificarClientes.java
@@ -0,0 +1,140 @@
+package paneles;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import clases.conexionBD;
+
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
+
+public class ModificarClientes extends JFrame {
+	
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textFieldNombre;
+	private JTextField textFieldApellido;
+	private JTextField textFieldTel;
+	private JTextField textFieldCorreo;
+	/**
+	 * Create the frame.
+	 */
+	public ModificarClientes(String[] arra) {
+		setTitle("Systemovie (Modificar clientes)");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 548, 546);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblModificarClientes = new JLabel("MODIFICAR CLIENTES");
+		lblModificarClientes.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		lblModificarClientes.setBounds(134, 13, 262, 36);
+		contentPane.add(lblModificarClientes);
+		
+		JLabel label_1 = new JLabel("Datos personales");
+		label_1.setBounds(12, 93, 120, 16);
+		contentPane.add(label_1);
+		
+		JLabel label_2 = new JLabel("Nombre:");
+		label_2.setBounds(12, 122, 120, 16);
+		contentPane.add(label_2);
+		
+		textFieldNombre = new JTextField();
+		textFieldNombre.setText(arra[0]);
+		textFieldNombre.setColumns(10);
+		textFieldNombre.setBounds(147, 122, 116, 22);
+		contentPane.add(textFieldNombre);
+		
+		JLabel label_3 = new JLabel("Apellido:");
+		label_3.setBounds(275, 125, 120, 16);
+		contentPane.add(label_3);
+		
+		textFieldApellido = new JTextField();
+		textFieldApellido.setText(arra[1]);
+		textFieldApellido.setColumns(10);
+		textFieldApellido.setBounds(404, 122, 116, 22);
+		contentPane.add(textFieldApellido);
+		
+		JLabel label_4 = new JLabel("Telefono:");
+		label_4.setBounds(12, 154, 120, 16);
+		contentPane.add(label_4);
+		
+		textFieldTel = new JTextField();
+		textFieldTel.setText(arra[2]);
+		textFieldTel.setColumns(10);
+		textFieldTel.setBounds(147, 151, 116, 22);
+		contentPane.add(textFieldTel);
+		
+		JLabel label_5 = new JLabel("Correo:");
+		label_5.setBounds(272, 154, 120, 16);
+		contentPane.add(label_5);
+		
+		textFieldCorreo = new JTextField();
+		textFieldCorreo.setText(arra[3]);
+		textFieldCorreo.setColumns(10);
+		textFieldCorreo.setBounds(404, 151, 116, 22);
+		contentPane.add(textFieldCorreo);
+		
+		JLabel label_9 = new JLabel("Membresia:");
+		label_9.setBounds(275, 221, 120, 16);
+		contentPane.add(label_9);
+		
+		JRadioButton radioButtonF = new JRadioButton("F");
+		radioButtonF.setBounds(404, 214, 45, 25);
+		contentPane.add(radioButtonF);
+		
+		JRadioButton radioButtonS = new JRadioButton("SF");
+		radioButtonS.setBounds(466, 214, 45, 25);
+		contentPane.add(radioButtonS);
+		
+		if(arra[7].equals("F"))
+			radioButtonF.setSelected(true);
+		else
+			radioButtonS.setSelected(true);
+		
+		//System.out.println(dias.charAt(i));
+		
+		JButton btnModificar = new JButton("Modificar");
+		btnModificar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				conexionBD conexionBD = new conexionBD();
+				
+				String nombre=textFieldNombre.getText();
+				String apellido=textFieldApellido.getText();
+				String telefono=textFieldTel.getText();
+				String correo=textFieldCorreo.getText();
+				String membresia="";
+				if(radioButtonF.isSelected()) {
+					membresia="F";
+				}else{
+					if(radioButtonS.isSelected()) {
+						membresia="SF";
+					}
+				}
+				
+				try {
+					conexionBD.modificarCliente(nombre, apellido, telefono, correo, arra[12]);
+				} catch (SQLException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		btnModificar.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnModificar.setBounds(202, 459, 126, 25);
+		contentPane.add(btnModificar);
+	}
+
+}
